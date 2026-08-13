52105ff25b3c8287fd975f81091e5fdf6346149b
ZAID-0310 <izaidraul1507@gmail.com>
2026-08-13 01:24:47 -0500

    comentario_prueba

--- database/migrations/2026_08_13_060350_create_income_table.php
+++ database/migrations/2026_08_13_060350_create_income_table.php
@@ -11,6 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
+
+    /**
+     * database creada
+     */
         Schema::create('income', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->nullable()
